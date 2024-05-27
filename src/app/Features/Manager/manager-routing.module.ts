df23aa112bd7a3d49a2e47af5be9689c3bdc14fb
IsrAhmad <Israa.099@gmail.com>
2024-05-27 23:04:26 +0300

    routing for manager and guest

--- src/app/Features/Manager/manager-routing.module.ts
+++ src/app/Features/Manager/manager-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManagerComponent } from './manager.component';
 
-const routes: Routes = [{ path: '', component: ManagerComponent }, { path: 'rooms', loadChildren: () => import('./modules/rooms/rooms.module').then(m => m.RoomsModule) }, { path: 'ads', loadChildren: () => import('./modules/ads/ads.module').then(m => m.AdsModule) }, { path: 'facilities', loadChildren: () => import('./modules/facilities/facilities.module').then(m => m.FacilitiesModule) }, { path: 'booking', loadChildren: () => import('./modules/booking/booking.module').then(m => m.BookingModule) }];
+const routes: Routes = [
+  { path: '', component: ManagerComponent, children: [
+    {path: '' , redirectTo: 'home' , pathMatch: 'full'},
+    { path: 'home',  loadChildren: () => import('../Manager/components/home/home.component').then(m => m.HomeComponent)},
+  ] },
+  { path: 'rooms', loadChildren: () => import('./modules/rooms/rooms.module').then(m => m.RoomsModule) },
+  { path: 'ads', loadChildren: () => import('./modules/ads/ads.module').then(m => m.AdsModule) },
+  { path: 'facilities', loadChildren: () => import('./modules/facilities/facilities.module').then(m => m.FacilitiesModule) },
+  { path: 'booking', loadChildren: () => import('./modules/booking/booking.module').then(m => m.BookingModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/Features/guest/guest-routing.module.ts
+++ src/app/Features/guest/guest-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GuestComponent } from './guest.component';
 
-const routes: Routes = [{ path: '', component: GuestComponent }, { path: 'payment', loadChildren: () => import('./modules/payment/payment.module').then(m => m.PaymentModule) }];
+const routes: Routes = [
+  { path: '', component: GuestComponent, children: [
+    {path: '' , redirectTo: 'home' , pathMatch: 'full'},
+    { path: 'home',  loadChildren: () => import('../guest/components/home/home.component').then(m => m.HomeComponent)},
+    { path: 'explore',  loadChildren: () => import('../guest/components/explore/explore.component').then(m => m.ExploreComponent)},
+    { path: 'favorite',  loadChildren: () => import('../guest/components/favorite/favorite.component').then(m => m.FavoriteComponent)},
+    { path: 'room-details',  loadChildren: () => import('../guest/components/room-details/room-details.component').then(m => m.RoomDetailsComponent)},
+  ] },
+  { path: 'payment', loadChildren: () => import('./modules/payment/payment.module').then(m => m.PaymentModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
