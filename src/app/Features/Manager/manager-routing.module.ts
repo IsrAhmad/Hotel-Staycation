f08185891dc29149d35cec3a267e360026c25533
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-05-29 23:56:40 +0300

    handling sidebar

--- src/app/Features/Manager/manager-routing.module.ts
+++ src/app/Features/Manager/manager-routing.module.ts
@@ -4,14 +4,18 @@ import { ManagerComponent } from './manager.component';
 
 const routes: Routes = [
   { path: '', component: ManagerComponent, children: [
-    {path: '' , redirectTo: 'home' , pathMatch: 'full'},
-    { path: 'home',  loadComponent: () => import('../Manager/components/home/home.component').then(m => m.HomeComponent)},
-  ] },
+ 
+    { path: 'home',  loadComponent: 
+    () => import('../Manager/components/home/home.component').then(m => m.HomeComponent)},
+  
   { path: 'rooms', loadChildren: () => import('./modules/rooms/rooms.module').then(m => m.RoomsModule) },
   { path: 'ads', loadChildren: () => import('./modules/ads/ads.module').then(m => m.AdsModule) },
-  { path: 'facilities', loadChildren: () => import('./modules/facilities/facilities.module').then(m => m.FacilitiesModule) },
-  { path: 'booking', loadChildren: () => import('./modules/booking/booking.module').then(m => m.BookingModule) }];
-
+  { path: 'facilities', loadChildren:
+   () => import('./modules/facilities/facilities.module').then(m => m.FacilitiesModule) },
+   { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
+  { path: 'booking', loadChildren: () => import('./modules/booking/booking.module').then(m => m.BookingModule) }]
+  },
+ ]
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+
+
+
 
 
 @NgModule({
   declarations: [
-    SharedComponent
+    SharedComponent,
+ SidebarComponent
   ],
   imports: [
     CommonModule,
-    SharedRoutingModule
-  ]
+    SharedRoutingModule,
+    RouterModule
+  ],
+  exports:[SidebarComponent]
 })
 export class SharedModule { }
