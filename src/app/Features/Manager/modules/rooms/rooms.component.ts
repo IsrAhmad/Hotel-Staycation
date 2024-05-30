7accdb945d992ba03ae2cca5eade980bc46f125f
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-05-30 21:30:28 +0300

    handling room table

--- src/app/Features/Manager/modules/rooms/rooms.component.ts
+++ src/app/Features/Manager/modules/rooms/rooms.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoomsService } from './services/rooms.service';
+import { IRoom, IRoomData } from './models/IRoom.model';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
+export class RoomsComponent  implements OnInit{
+ 
+  //for header
+  btnText :string = 'Add new room' ;
+  headerText:string ='Rooms Table Details' ;
+  headerPargraph:string ='You can check all details';
+  addRoomLink:string =''
 
+ 
+  displayedColumns: string[] = ['Room number','Images' ,'Discount' 
+  ,'Capacity', 'Created at' ,'Created by' , 'Actions'];
+
+  roomData:IRoom[]=[]
+  constructor(private _RoomsService:RoomsService){}
+  ngOnInit(): void {
+     this.getAllRooms();
+  }
+
+
+  ////
+  getAllRooms(){
+    let params= {
+      page :1 ,
+      size:10
+
+    }
+    this._RoomsService.getAllRooms(params).subscribe({
+     next:(res )=>{
+      console.log(res);
+      this.roomData= res.data.rooms
+      console.log(this.roomData);
+
+     }      
+    })
+  }
 }
