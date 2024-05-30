d25df7fcbe7834ce3dda8f656eac5f4886dc46a6
IsrAhmad <Israa.099@gmail.com>
2024-05-30 20:02:55 +0300

    code review

--- src/app/Core/auth/services/auth.service.ts
+++ src/app/Core/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { IResetRequest, IResetResponse } from './../model/reset-password';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { ILogIn } from '../model/ILogin';
@@ -14,9 +14,11 @@ import { IRegister } from '../model/IRegister.model';
   providedIn: 'root'
 })
 export class AuthService {
+  role: string | null = null;
 
-  constructor(private _HttpClient:HttpClient) { }
-
+  constructor(private _HttpClient:HttpClient) {
+    this.getRole();
+  }
 
   login(userData:FormGroup):Observable<ILogIn>{
     return this._HttpClient.post<ILogIn>('admin/users/login',userData)
@@ -35,6 +37,15 @@ export class AuthService {
 
   register(userData:FormData):Observable<IRegister>{
     return this._HttpClient.post<IRegister>('admin/users',userData)
+  }
+  getRole() {
+    const token = localStorage.getItem('token');
+    const role = localStorage.getItem('role');
+
+    if (token !== null && role !== null) {
+      this.role = role;
+    } else {
+    }
   }
 
   }
--- src/app/shared/not-found/not-found.component.ts
+++ src/app/shared/not-found/not-found.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Core/auth/services/auth.service';
 
 @Component({
   selector: 'app-not-found',
@@ -11,11 +12,18 @@ import { Router } from '@angular/router';
 })
 export class NotFoundComponent {
 
-  constructor(private _Router:Router) {}
+  constructor(private _Router:Router, private _AuthService: AuthService) { }
+
+  OnInit() {
+    this._AuthService.getRole();
+  }
+
+  isAdmin(): boolean {
+    return this._AuthService.role === 'admin';
+  }
 
   goHome() {
-    this._Router.navigate(['/manager/home']);
-    console.log('Home');
+    this.isAdmin() ? this._Router.navigate(['/manager/home']) : this._Router.navigate(['/guest/home']);
   }
 
 }
