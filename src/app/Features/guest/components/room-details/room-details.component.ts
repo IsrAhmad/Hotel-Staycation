ad07c6b25b051c77389360bd8e90a3114bdbc8cf
Esraa2410 <esraaaragabb@gmail.com>
2024-06-07 19:52:54 -0700

    link home with roomDtails

--- src/app/Features/guest/components/room-details/room-details.component.ts
+++ src/app/Features/guest/components/room-details/room-details.component.ts
@@ -1,4 +1,4 @@
-import { IRommCommentResponse, IRoomReveiwResponse ,IRoomReveiwRequest} from './../../models/room-details';
+import { IRommCommentResponse, IRoomReveiwResponse, IRoomReveiwRequest } from './../../models/room-details';
 import { Component } from '@angular/core';
 import { CommonModule, formatDate } from '@angular/common';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
@@ -7,12 +7,13 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { RoomDetailsService } from '../../services/room-details.service';
 import { ToastrService } from 'ngx-toastr';
 import { StarRatingModule, StarRatingConfigService } from 'angular-star-rating';
+import { ActivatedRoute } from '@angular/router';
 
 
 
@@ -31,14 +32,21 @@ export class RoomDetailsComponent {
   year = this.today.getFullYear();
   roomRes: any;
   rating: number = 0;
+   id: string = '';
 
-  constructor (private _FormBuilder:FormBuilder, private _HttpClient: HttpClient, private _RoomDetailsService: RoomDetailsService, private _ToastrService: ToastrService) {
+  constructor(private _ActivatedRoute: ActivatedRoute, private _HttpClient: HttpClient, private _RoomDetailsService: RoomDetailsService, private _ToastrService: ToastrService) {
+
 
   }
   ngOnInit(): void {
-    this.getRoomById(this.id);
+    if (this._ActivatedRoute.snapshot.params['id']) {
+     this.id= this._ActivatedRoute.snapshot.params['id'];
+      console.log('id' + this.id);
+      this.getRoomById(this.id);
+    }
+
   }
-  id: string = '65aecf82e815336ace213ab7';
+ 
 
   reviewForm: FormGroup = new FormGroup({
     roomId: new FormControl(this.id, [Validators.required]),
@@ -47,7 +55,7 @@ export class RoomDetailsComponent {
   });
 
 
-  onAddReview( reviewForm: FormGroup) {
+  onAddReview(reviewForm: FormGroup) {
     this._RoomDetailsService.AddRoomreview(reviewForm.value).subscribe({
       next: (res: IRoomReveiwResponse) => {
         //console.log(res);
@@ -72,7 +80,7 @@ export class RoomDetailsComponent {
   onAddComment(commentForm: FormGroup) {
     this._RoomDetailsService.AddRoomComment(commentForm).subscribe({
       next: (res: IRommCommentResponse) => {
-       // console.log(res);
+        // console.log(res);
       },
       error: (err: HttpErrorResponse) => {
         //console.log(err);
@@ -127,8 +135,8 @@ export class RoomDetailsComponent {
     end: new FormControl(new Date(this.year, this.month, 16)),
   });
 
-  getDiscountedPrice(price: number, discount: number ,capicity:number): number {
-    return (price - discount)*capicity;
+  getDiscountedPrice(price: number, discount: number, capicity: number): number {
+    return (price - discount) * capicity;
   }
 
 
