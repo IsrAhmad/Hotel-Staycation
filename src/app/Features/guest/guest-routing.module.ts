ce3ae6b6897a53c8ff23f5f4415918659d6e364e
Esraa2410 <esraaaragabb@gmail.com>
2024-06-10 04:16:50 -0700

    contact-us-component

--- src/app/Features/guest/guest-routing.module.ts
+++ src/app/Features/guest/guest-routing.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GuestComponent } from './guest.component';
 import { ViewUserProfileComponent } from 'src/app/shared/components/view-user-profile/view-user-profile.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 const routes: Routes = [
-  { path: '', component: GuestComponent, children: [
-    {path: '' , redirectTo: 'home' , pathMatch: 'full'},
-    { path: 'home',  loadComponent: () => import('../guest/components/home/home.component').then(m => m.HomeComponent)},
-    { path: 'explore',  loadComponent: () => import('../guest/components/explore/explore.component').then(m => m.ExploreComponent)},
-    { path: 'favorite',  loadComponent: () => import('../guest/components/favorite/favorite.component').then(m => m.FavoriteComponent)},
-    { path: 'room-details/:id',  loadComponent: () => import('../guest/components/room-details/room-details.component').then(m => m.RoomDetailsComponent)},
-
-  { path: 'payment', loadChildren: () => import('./modules/payment/payment.module').then(m => m.PaymentModule) },
   {
-    path:'profile/:id' , component:ViewUserProfileComponent
-  }
-] },
+    path: '', component: GuestComponent, children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', loadComponent: () => import('../guest/components/home/home.component').then(m => m.HomeComponent) },
+      { path: 'explore', loadComponent: () => import('../guest/components/explore/explore.component').then(m => m.ExploreComponent) },
+      { path: 'favorite', loadComponent: () => import('../guest/components/favorite/favorite.component').then(m => m.FavoriteComponent) },
+      { path: 'room-details/:id', loadComponent: () => import('../guest/components/room-details/room-details.component').then(m => m.RoomDetailsComponent) },
+      { path: 'contact-us', component: ContactUsComponent },
+      { path: 'payment', loadChildren: () => import('./modules/payment/payment.module').then(m => m.PaymentModule) },
+      {
+        path: 'profile/:id', component: ViewUserProfileComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
--- src/app/Features/guest/guest.module.ts
+++ src/app/Features/guest/guest.module.ts
@@ -5,6 +5,7 @@ import { GuestRoutingModule } from './guest-routing.module';
 import { GuestComponent } from './guest.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { UpDateCommentComponent } from './components/room-details/components/up-date-comment/up-date-comment.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 
 
@@ -12,7 +13,8 @@ import { UpDateCommentComponent } from './components/room-details/components/up-
 @NgModule({
   declarations: [
     GuestComponent,
-    UpDateCommentComponent
+    UpDateCommentComponent,
+    ContactUsComponent
   ],
   imports: [
     CommonModule,
