18df1a20c17035aeea419c2d44c5881fb4acbd74
yasmingamil22 <yasmingamy@gmail.com>
2024-06-10 17:22:47 +0300

    msg

--- src/app/Core/auth/auth.component.ts
+++ src/app/Core/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
+
+  lang: string = localStorage.getItem('lang') !== null ? localStorage.getItem('lang')! : 'en';
+
+  constructor(private translate: TranslateService) {
+    //console.log(this.lang)
+  
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      // do something
+      //console.log(event)
+      this.lang=event.lang
+    });
+
+    this.changeLanguage(this.lang)
+
+}
+
+changeLanguage(val:string):void{
+  this.translate.setDefaultLang(val);
+  this.translate.use(val);
+  this.lang=val;
+  localStorage.setItem('lang',this.lang)
+
+}
 
 }
