57edb7cd086e9730bc4567b3a6b93f5bc4b897cc
IsrAhmad <Israa.099@gmail.com>
2024-06-12 18:47:47 +0300

    privacy policy & data deletion

---
+++ src/app/Core/auth/components/data-deletion/data-deletion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-data-deletion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data-deletion.component.html',
+  styleUrls: ['./data-deletion.component.scss']
+})
+export class DataDeletionComponent {
+
+}
