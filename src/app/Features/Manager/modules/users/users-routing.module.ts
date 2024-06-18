3eb45abe7e8e5d71e7594849348a4b40195e5cc6
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-06-18 14:47:44 +0300

    updating users routing module

--- src/app/Features/Manager/modules/users/users-routing.module.ts
+++ src/app/Features/Manager/modules/users/users-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersListComponent } from './components/users-list/users-list.component';
 import { AddAdminComponent } from './components/add-admin/add-admin.component';
 import { UsersComponent } from './users.component';
 
