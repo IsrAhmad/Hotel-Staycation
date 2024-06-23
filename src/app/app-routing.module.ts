e5cf013d3f700302be03fcdd37bbc516e4e5e30c
IsrAhmad <Israa.099@gmail.com>
2024-06-23 23:37:37 +0300

    (routes, { useHash: true })

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
